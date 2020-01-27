35d291b9f40c85691d1dadd0f3de84b6bc3f2a6b
Joel Dahne <joel@dahne.eu>
2020-01-27 20:13:53 +0100

    Add tests

---
+++ test/runtests.jl
@@ -0,0 +1,8 @@
+using MPS
+using Nemo
+using Test
+
+include("triangles.jl")
+
+include("regulartriangles.jl")
+include("singulartriangles.jl")
---
+++ test/singulartriangles.jl
@@ -0,0 +1,28 @@
+@testset "singular triangles" begin
+    N = 16
+    prec = 64
+
+    RR = RealField(prec)
+
+    old_prec = precision(BigFloat)
+    setprecision(BigFloat, prec)
+
+    results = RR.(["4.3 +/- 0.0813",
+                   "5.16 +/- 7.74e-3",
+                   "6.2 +/- 0.0849",
+                   "6.78 +/- 8.61e-3"])
+
+    for i in 1:4
+        domain, interval = triangle(6 + i)
+        u = SphericalVertexEigenfunction(domain, 1)
+
+        λ, u = mps(domain, u, interval, N)
+
+        @test overlaps(results[i], λ)
+        @show results[i]
+        @show λ
+        @show Float64(radius(λ))
+    end
+
+    setprecision(BigFloat, old_prec)
+end
