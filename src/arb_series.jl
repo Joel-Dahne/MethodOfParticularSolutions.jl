16b32fb4ab1e24b0bff4dd6f271194e7209220b8
Joel Dahne <joel@dahne.eu>
2020-02-26 17:32:16 +0100

    Use nested if statements for series of Bessel and Legendre

--- src/arb_series.jl
+++ src/arb_series.jl
@@ -13,49 +13,54 @@ function bessel_j(ν::arb, z::arb_series, n = length(z))
         x = z[0]
         a0 = bessel_j(ν, x)
         res[0] = a0
-    end
 
-    if !isfinite(a0)
-        return arb_series(parent(z.poly)(fill(NaN, n)), n)
-    end
+        if !isfinite(a0)
+            return arb_series(parent(z.poly)(fill(NaN, n)), n)
+        end
 
-    if n > 1
-        a1 = 1//2*(bessel_j(ν - 1, x) - bessel_j(ν + 1, x))
-        res[1] = a1
-    end
+        if n > 1
+            a1 = 1//2*(bessel_j(ν - 1, x) - bessel_j(ν + 1, x))
+            res[1] = a1
 
-    if !isfinite(a0)
-        return arb_series(parent(z.poly)(fill(NaN, n)), n)
-    end
+            if !isfinite(a0)
+                return arb_series(parent(z.poly)(fill(NaN, n)), n)
+            end
 
-    if n > 2
-        a2 = 1//8*(bessel_j(ν - 2, x) + bessel_j(ν + 2, x) - 2a0)
-        res[2] = a2
-    end
+            if n > 2
+                a2 = 1//8*(bessel_j(ν - 2, x) + bessel_j(ν + 2, x) - 2a0)
+                res[2] = a2
 
-    if !isfinite(a0)
-        return arb_series(parent(z.poly)(fill(NaN, n)), n)
-    end
 
-    if n > 3
-        a3 = 1//48*(bessel_j(ν - 3, x) - bessel_j(ν + 3, x) - 6a1)
-        res[3] = a3
-    end
+                if !isfinite(a0)
+                    return arb_series(parent(z.poly)(fill(NaN, n)), n)
+                end
 
-    if !isfinite(a0)
-        return arb_series(parent(z.poly)(fill(NaN, n)), n)
-    end
+                if n > 3
+                    a3 = 1//48*(bessel_j(ν - 3, x) - bessel_j(ν + 3, x) - 6a1)
+                    res[3] = a3
 
-    for i in 4:n-1
-        k = i - 4
-        ai = -(res[k]
-               + 2x*res[k + 1]
-               + (k^2 - ν^2 + x^2 + 4k + 4)*res[k + 2]
-               + (2k^2 + 11k + 15)*x*res[k + 3]
-               )/(x^2*(k^2 + 7k + 12))
-        res[i] = ai
-    end
 
+                    if !isfinite(a0)
+                        return arb_series(parent(z.poly)(fill(NaN, n)), n)
+                    end
+
+                    if n > 4
+                        x2 = x^2
+                        ν2 = ν^2
+                        for i in 4:n-1
+                            k = i - 4
+                            ai = -(res[k]
+                                   + 2x*res[k + 1]
+                                   + (k^2 + 4k + 4 - ν2 + x2)*res[k + 2]
+                                   + (2k^2 + 11k + 15)*x*res[k + 3]
+                                   )/(x2*(k^2 + 7k + 12))
+                            res[i] = ai
+                        end
+                    end
+                end
+            end
+        end
+    end
     # Compose the Taylor series for the Bessel function with that of z
     z_tmp = arb_series(deepcopy(z.poly))
     z_tmp[0] = base_ring(parent(z.poly))(0)
@@ -107,54 +112,55 @@ function legendre_p(ν::arb, μ::arb, z::arb_series, n = length(z))
         x = z[0]
         a0 = legendre_p_safe(ν, μ, x)
         res[0] = a0
+
+        if !isfinite(a0)
+            return arb_series(parent(z.poly)(fill(NaN, n)), n)
+        end
+
+        if n > 1
+            x2 = x^2
+            onemx2 = 1 - x2
+
+            a1 = ((ν + 1)*x*a0 - (ν - μ + 1)*legendre_p_safe(ν + 1, μ, x))/(onemx2)
+            res[1] = a1
+
+            if !isfinite(a1)
+                return arb_series(parent(z.poly)(fill(NaN, n)), n)
+            end
+
+            if n > 2
+                ν2 = ν^2
+                μ2 = μ^2
+
+                a2 = (2x*a1 - (ν2 + ν - μ2/(onemx2))*a0)/(2(onemx2))
+                res[2] = a2
+
+                if n > 3
+                    a3 = (4(onemx2)^2*x*a2
+                          + 2(2μ2 - (ν2 + ν)*(onemx2))*x*a0
+                          + ((ν2 + ν + 2)*x2 + μ2 - ν2 - ν + 2)*(onemx2)*a1)/(6*(onemx2)^3)
+                    res[3] = a3
+
+                    if n > 4
+                        onemx22 = onemx2^2
+                        xonemx2 = x*onemx2
+                        ν2pν = ν2 + ν
+                        μ2mν2pν = μ2 - ν2pν
+                        fourx = 4x
+                        for i in 4:n-1
+                            k = i - 4
+                            ai = ((k + 1 + ν)*(ν - k)*res[k]
+                                  - (0.5*(2k^2 + (5k + 3) - ν2pν))*fourx*res[k + 1]
+                                  + (((-6k^2 - 24k - 24) + ν2pν)*x2 + (2k^2 + 8k + 8) + μ2mν2pν)*res[k + 2]
+                                  + (2(k + 3)*(2k + 5))*xonemx2*res[k + 3]
+                                  )/(onemx22*((k + 4)*(k + 3)))
+                            res[i] = ai
+                        end
+                    end
+                end
+            end
+        end
     end
-
-    if !isfinite(a0)
-        return arb_series(parent(z.poly)(fill(NaN, n)), n)
-    end
-
-    x2 = x^2
-    onemx2 = 1 - x2
-
-    if n > 1
-        a1 = ((ν + 1)*x*a0 - (ν - μ + 1)*legendre_p_safe(ν + 1, μ, x))/(onemx2)
-        res[1] = a1
-    end
-
-    if !isfinite(a1)
-        return arb_series(parent(z.poly)(fill(NaN, n)), n)
-    end
-
-    ν2 = ν^2
-    μ2 = μ^2
-
-    if n > 2
-        a2 = (2x*a1 - (ν2 + ν - μ2/(onemx2))*a0)/(2(onemx2))
-        res[2] = a2
-    end
-
-    if n > 3
-        a3 = (4(onemx2)^2*x*a2
-              + 2(2μ2 - (ν2 + ν)*(onemx2))*x*a0
-              + ((ν2 + ν + 2)*x2 + μ2 - ν2 - ν + 2)*(onemx2)*a1)/(6*(onemx2)^3)
-        res[3] = a3
-    end
-
-    onemx22 = onemx2^2
-    xonemx2 = x*onemx2
-    ν2pν = ν2 + ν
-    μ2mν2pν = μ2 - ν2pν
-    fourx = 4x
-    for i in 4:n-1
-        k = i - 4
-        ai = ((k + 1 + ν)*(ν - k)*res[k]
-              - (0.5*(2k^2 + (5k + 3) - ν2pν))*fourx*res[k + 1]
-              + (((-6k^2 - 24k - 24) + ν2pν)*x2 + (2k^2 + 8k + 8) + μ2mν2pν)*res[k + 2]
-              + (2(k + 3)*(2k + 5))*xonemx2*res[k + 3]
-              )/(onemx22*((k + 4)*(k + 3)))
-        res[i] = ai
-    end
-
     # Compose the Taylor series for the Legendre function with that of
     # z
     z_tmp = arb_series(deepcopy(z.poly))
