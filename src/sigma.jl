feca7c5730456102a6ec4a95c106a645e2624c82
Joel Dahne <joel@dahne.eu>
2020-04-22 10:47:14 +0200

    Factor out computation of the matrix in sigma

--- src/sigma.jl
+++ src/sigma.jl
@@ -1,3 +1,32 @@
+function sigma_matrix(λ,
+                      domain::AbstractDomain,
+                      eigenfunction::AbstractEigenfunction,
+                      N::Integer;
+                      num_boundary::Integer = 2N,
+                      num_interior::Integer = 2N)
+    @timeit_debug "points" begin
+        # Compute boundary and interior points of the domain
+        boundary, boundary_index = boundary_points(domain, eigenfunction, num_boundary)
+        interior = interior_points(domain, num_interior)
+
+    end
+
+    @timeit_debug "matrix" begin
+        A = Array{BigFloat}(undef, num_interior + num_boundary, N)
+        for k in 1:N
+            for i in 1:num_boundary
+                A[i, k] = BigFloat(eigenfunction(boundary[i], domain.parent(λ), k, boundary = boundary_index[i]))
+            end
+
+            for i in 1:num_interior
+                A[num_boundary + i, k] = BigFloat(eigenfunction(interior[i], domain.parent(λ), k))
+            end
+        end
+    end
+
+    A
+end
+
 """
     sigma(λ, ...)
 > Return σ(λ).
@@ -9,26 +38,10 @@ function sigma(λ,
                num_boundary::Integer = 2N,
                num_interior::Integer = 2N)
     @timeit_debug "sigma" begin
-
-        @timeit_debug "points" begin
-            # Compute boundary and interior points of the domain
-            boundary, boundary_index = boundary_points(domain, eigenfunction, num_boundary)
-            interior = interior_points(domain, num_interior)
-
-        end
-
-        @timeit_debug "matrix" begin
-            A = Array{BigFloat}(undef, num_interior + num_boundary, N)
-            for k in 1:N
-                for i in 1:num_boundary
-                    A[i, k] = BigFloat(eigenfunction(boundary[i], domain.parent(λ), k, boundary = boundary_index[i]))
-                end
-
-                for i in 1:num_interior
-                    A[num_boundary + i, k] = BigFloat(eigenfunction(interior[i], domain.parent(λ), k))
-                end
-            end
-        end
+        # Compute the matrix A
+        A = sigma_matrix(λ, domain, eigenfunction, N,
+                         num_boundary = num_boundary,
+                         num_interior = num_interior)
 
         # Compute a QR factorization of A
         @timeit_debug "qr" Q = Matrix(qr(A).Q)
@@ -50,26 +63,10 @@ function sigma_coefficients(λ,
                             num_boundary::Integer = 2N,
                             num_interior::Integer = 2N)
     @timeit_debug "sigma_coefficients" begin
-
-        @timeit_debug "points" begin
-            # Compute boundary and interior points of the domain
-            boundary, boundary_index = boundary_points(domain, eigenfunction, num_boundary)
-            interior = interior_points(domain, num_interior)
-        end
-
-        @timeit_debug "matrix" begin
-            # Evaluate the basis of the eigenfunction on the points
-            A = Array{BigFloat}(undef, num_interior + num_boundary, N)
-            for k in 1:N
-                for i in 1:num_boundary
-                    A[i, k] = BigFloat(eigenfunction(boundary[i], domain.parent(λ), k, boundary = boundary_index[i]))
-                end
-
-                for i in 1:num_interior
-                    A[num_boundary + i, k] = BigFloat(eigenfunction(interior[i], domain.parent(λ), k))
-                end
-            end
-        end
+        # Compute the matrix A
+        A = sigma_matrix(λ, domain, eigenfunction, N,
+                         num_boundary = num_boundary,
+                         num_interior = num_interior)
 
         @timeit_debug "qr" begin
             # Compute a QR factorization of A
