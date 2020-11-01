80c4351d09f082656fed40f7e8ed54a2c2897f0e
Joel Dahne <joel@dahne.eu>
2020-11-01 18:09:31 +0100

    Fix nu for VertexEigenfunction

--- src/eigenfunction/VertexEigenfunction.jl
+++ src/eigenfunction/VertexEigenfunction.jl
@@ -20,7 +20,7 @@ end
 
 Return `k*ν₀` as an `arb`, the parameter used for the Bessel function.
 """
-function nu(u::VertexEigenfunction{fmpq}, k::Integer = 1)
+function nu(u::VertexEigenfunction, k::Integer = 1)
     u.domain.parent(-k*inv(angledivπ(u.domain, u.vertex)))
 end
 
