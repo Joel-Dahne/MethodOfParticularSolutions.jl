ef6976f5a803211d744ba23d492f14f2737739d1
Joel Dahne <joel@dahne.eu>
2021-07-22 20:55:11 +0200

    Rename tests
    This is to differentiate between tests which are more like unit tests
    and tests which are most end to end tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,10 @@
 using MethodOfParticularSolutions, Nemo, Printf, Test
 
 @testset "MethodOfParticularSolutions" begin
-    include("sphericaltriangle.jl")
-    include("lshape.jl")
-    include("triangles.jl")
-    include("example-domains.jl")
+    @testset "end-to-end-tests" begin
+        include("end-to-end-tests/sphericaltriangle.jl")
+        include("end-to-end-tests/lshape.jl")
+        include("end-to-end-tests/triangles.jl")
+        include("end-to-end-tests/example-domains.jl")
+    end
 end
